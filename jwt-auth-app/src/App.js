3438f9bb77c344174dff3d572c3454068e20694e
Noorullah <itxnoor148@gmail.com>
2026-04-21 22:40:44 +0500

    Day 21: Added lazy loading and suspense

--- jwt-auth-app/src/App.js
+++ jwt-auth-app/src/App.js
@@ -1,20 +1,33 @@
-import React, { useContext } from 'react';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import React, { useContext, lazy, Suspense } from 'react';
 import { AuthContext } from './context/AuthContext';
+
+const Login = lazy(() =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(import('./pages/Login')), 1000); // 800ms delay
+  })
+);
+
+const Dashboard = lazy(() =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(import('./pages/Dashboard')), 1000);
+  })
+);
 
 function App() {
   const { user, loading } = useContext(AuthContext);
 
   if (loading) {
-    return <h2>Loading...</h2>;
+    return <h2>Checking authentication...</h2>;
   }
 
   return (
     <div className="app">
-      <h1>JWT Authentication System </h1>
-      
-      {user ? <Dashboard /> : <Login />}
+      <h1>JWT Authentication System</h1>
+
+      {/* Suspense handles lazy loading UI */}
+      <Suspense fallback={<h2>Loading page...</h2>}>
+        {user ? <Dashboard /> : <Login />}
+      </Suspense>
     </div>
   );
 }
